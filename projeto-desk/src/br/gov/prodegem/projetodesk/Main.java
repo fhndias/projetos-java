222f487abc57412d504f7c87e6a625a7f86ca4f1
fhndias <fhndias02@gmail.com>
2017-02-02 15:49:27 -0200

    Remoção de todas as linhas

--- projeto-desk/src/br/gov/prodegem/projetodesk/Main.java
+++ projeto-desk/src/br/gov/prodegem/projetodesk/Main.java
@@ -2,12 +2,6 @@ package br.gov.prodegem.projetodesk;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("Brave New World!");
-		System.out.println("Taca lepal!");
-		System.out.println("Lero-lero");
-		System.out.println("Batatinha");
-		System.out.println("Bola");
-		System.out.println("Pedra");
+
 	}
 }	
