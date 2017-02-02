ecbdc8854f7e97446296b7434fe838b1099380dd
Fernando Dias <fhndias02@gmail.com>
2017-02-02 13:59:59 -0300

    Adição de uma nova linha
    Adição de uma nova linha

--- projeto-desk/src/br/gov/prodegem/projetodesk/Main.java
+++ projeto-desk/src/br/gov/prodegem/projetodesk/Main.java
@@ -3,5 +3,6 @@ package br.gov.prodegem.projetodesk;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("Brave New World!");
 	}
 }	
