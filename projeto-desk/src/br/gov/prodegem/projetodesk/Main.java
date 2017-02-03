ebefffeef3f94ca716d2afeeef1c52944de0c755
fhndias <fhndias02@gmail.com>
2017-02-03 15:10:41 -0200

    Adição da linha inicial do projeto

--- projeto-desk/src/br/gov/prodegem/projetodesk/Main.java
+++ projeto-desk/src/br/gov/prodegem/projetodesk/Main.java
@@ -2,46 +2,6 @@ package br.gov.prodegem.projetodesk;
 
 public class Main {
 	public static void main(String[] args) {			
-		exibirMensagem1();
-		exibirMensagem2();
-		exibirMensagem3();
-		exibirMensagem4();
-		exibirMensagem5();
-		exibirMensagem6();
-		exibirMensagem7();
-		exibirMensagem8();
-	}
-	
-	private static void exibirMensagem1() {
-		System.out.println("Linha 10");
-	}
-	
-	
-	private static void exibirMensagem2() {
-		System.out.println("Linha 11");
-	}
-	
-	private static void exibirMensagem3() {
-		System.out.println("Linha 12");
-	}
-	
-	private static void exibirMensagem4() {
-		System.out.println("Linha 13");
-	}
-	
-	private static void exibirMensagem5() {
-		System.out.println("Linha 14");
-	}
-	
-	private static void exibirMensagem6() {
-		System.out.println("Linha 15");
-	}
-	
-	private static void exibirMensagem7() {
-		System.out.println("Linha 16");
-	}	
-	
-	private static void exibirMensagem8() {
-		System.out.println("Linha 17");
+		System.out.println("Linha inicial do master");
 	}
 }	
