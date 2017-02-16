1269c9dd72643b58afe55867211c6bea16d34c4b
fhndias <fhndias02@gmail.com>
2017-02-16 17:02:30 -0200

    Reset do método main

--- projeto-desk/src/br/gov/prodegem/projetodesk/Main.java
+++ projeto-desk/src/br/gov/prodegem/projetodesk/Main.java
@@ -2,8 +2,6 @@ package br.gov.prodegem.projetodesk;
 
 public class Main {
 	public static void main(String[] args) {			
-		System.out.println("Linha inicial do master");
-		System.out.println("Linha inicial (mudanca-01)");
-		System.out.println("Linha final (mudanca-01)");
+
 	}
 }	
