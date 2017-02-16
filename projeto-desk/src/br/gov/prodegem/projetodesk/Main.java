1ab71c3a058751dcfc05a44190b40c1f2a6fac5a
fhndias <fhndias02@gmail.com>
2017-02-15 22:34:45 -0200

    Reset do método main
    Exclusão de todas as linhas de código do método main.

--- projeto-desk/src/br/gov/prodegem/projetodesk/Main.java
+++ projeto-desk/src/br/gov/prodegem/projetodesk/Main.java
@@ -2,13 +2,6 @@ package br.gov.prodegem.projetodesk;
 
 public class Main {
 	public static void main(String[] args) {		
-		System.out.println("Linha 1");
-		System.out.println("Linha 2");
-		System.out.println("Linha 3");
-		System.out.println("Linha 4");
-		System.out.println("Linha 5");
-		System.out.println("Linha 6");
-		System.out.println("Linha 7");
-		System.out.println("Linha 8");
+
 	}
 }	
