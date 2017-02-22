35b2980d2eee4159080d5df4c022577246246727
fhndias <fhndias02@gmail.com>
2017-02-22 17:27:54 -0300

    Refatoração do menu

--- projeto-desk/src/br/gov/prodegem/projetodesk/Main.java
+++ projeto-desk/src/br/gov/prodegem/projetodesk/Main.java
@@ -24,13 +24,13 @@ public class Main {
  	        opcao = scanner.nextLine(); 	        
  	        
 			if (opcao.equals("1")) {
-				cadastroDeCliente();				
+				apresentarMenuCliente();				
 			} else if (opcao.equals("2")) {
-				cadastroDeCarro();
+
 			} else if (opcao.equals("3")) {
-				realizarEmprestimoDeCarro();
+
 			} else if (opcao.equals("4")) {
-				realizarDevolucaoDeCarro();
+
 			}
 			
 		}
@@ -38,9 +38,35 @@ public class Main {
 		scanner.close();
 	}
 	
-	private static void cadastroDeCliente() {
-		System.out.println("========== Cadastro de Cliente ==========");
+	private static void apresentarMenuCliente() {
+		String opcao = "0";		
 		
+		while (!opcao.equals("5")) {
+			System.out.println("+===================   Cliente   ===================+");
+			System.out.println("=              1 - Cadastrar Cliente                =");
+			System.out.println("=              2 - Atualizar Cliente                =");
+			System.out.println("=              3 - Pesquisar Cliente                =");
+			System.out.println("=              4 - Excluir Cliente                  =");
+			System.out.println("=              5 - Voltar                           =");
+			System.out.println("+===================================================+");
+			System.out.println("+===== Informe a opção: ");			
+			
+ 	        opcao = scanner.nextLine(); 	        
+ 	        
+			if (opcao.equals("1")) {
+				cadastrarCliente();			
+			} else if (opcao.equals("2")) {
+				atualizarCliente();
+			} else if (opcao.equals("3")) {
+				pesquisarCliente();
+			} else if (opcao.equals("4")) {
+				excluirCliente();
+			}
+			
+		}
+	}
+	
+	private static void cadastrarCliente() {
 		Cliente cliente = new Cliente();
 		
 		System.out.println("Informe o nome: ");
@@ -62,26 +88,59 @@ public class Main {
 		ClienteDAO dao = new ClienteDAO();
 		cliente = dao.salvar(cliente);
 		
-		System.out.println();
-		System.out.println("Cliente cadastrado: ");
 		System.out.println("Id: " + cliente.getId());
-		System.out.println("Nome: " + cliente.getNome());
-		System.out.println("CPF: " + cliente.getCpf());
-		System.out.println("Data de nascimento: " + cliente.getDataDeNascimento());
-		System.out.println("Endereço: " + cliente.getEndereco());
-		System.out.println("");
 	}
 	
-	private static void cadastroDeCarro() {
-		System.out.println("Cadastro de Carro...");
+	private static void atualizarCliente() {
+		System.out.println("Informe o id: ");
+		String id = scanner.nextLine();
+		
+		ClienteDAO dao = new ClienteDAO();
+		Cliente cliente = dao.pesquisar(id);
+		
+		System.out.println("Informe o nome: ");
+		String nome = scanner.nextLine();
+		cliente.setNome(nome);
+		
+		System.out.println("Informe o CPF: ");
+		String cpf = scanner.nextLine();
+		cliente.setCpf(cpf);
+		
+		System.out.println("Infome a data de nascimento: ");
+		String dataDeNascimento = scanner.nextLine();
+		cliente.setDataDeNascimento(dataDeNascimento);
+		
+		System.out.println("Informe o endereço: ");
+		String endereco = scanner.nextLine();
+		cliente.setEndereco(endereco);
+		
+		cliente = dao.atualizar(cliente);
 	}
 	
-	private static void realizarEmprestimoDeCarro() {
-		System.out.println("Realizando empréstimo...");
+	private static void pesquisarCliente() {
+		System.out.println("Informe o id do cliente: ");
+		String id = scanner.nextLine();
+		
+		ClienteDAO dao = new ClienteDAO();
+		Cliente cliente = dao.pesquisar(id);
+		
+		if (cliente != null) {
+			System.out.println("Id: " + cliente.getId());
+			System.out.println("Nome: " + cliente.getNome());
+			System.out.println("CPF: " + cliente.getCpf());
+			System.out.println("Data de Nascimento: " + cliente.getDataDeNascimento());
+			System.out.println("Endereço: " + cliente.getEndereco());
+		} else {
+			System.out.println("Cliente não encontrado!");
+		}
 	}
 	
-	private static void realizarDevolucaoDeCarro() {
-		System.out.println("Realizando devolução de carro...");
+	private static void excluirCliente() {
+		System.out.println("Informe o id do cliente: ");
+		String id = scanner.nextLine();
+		
+		ClienteDAO dao = new ClienteDAO();
+		dao.excluir(id); 
 	}
 
 }	
